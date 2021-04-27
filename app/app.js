f5e3a568a76dae440d22da9870db8c26e62aed87
EunhaKyeong <eh0634a@gmail.com>
2021-04-27 09:20:54 +0900

    폴더 구조 최적화

--- app/app.js
+++ app/app.js
@@ -5,10 +5,10 @@ const express = require('express');
 const app = express();
 
 //routing
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 //app setting
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");  //html이라고 생각하면 됨.
 
 //use -> middleware를 등록해주는 메서드
