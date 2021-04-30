66bca14f2275dc61d3f0803106119bd710e1b23c
EunhaKyeong <eh0634a@gmail.com>
2021-04-30 11:41:17 +0900

    로그 관리 모듈 morgan, winston 등록 및 설정

--- app/app.js
+++ app/app.js
@@ -3,18 +3,24 @@
 //module
 const express = require('express');
 const dotenv = require("dotenv");
-dotenv.config();
+//const morgan = require("morgan");
 
 const app = express();
+dotenv.config();
 
 //routing
 const home = require("./src/routes/home");
+
+//const accessLogStream = require("./src/config/log");
+const logger = require("./src/config/logger");
 
 //use -> middleware를 등록해주는 메서드
 //URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결.
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
 app.use(express.static(`${__dirname}/src/public`));
+//app.use(morgan('dev'));
+//app.use(morgan('common', { stream: accessLogStream }));
 app.use("/", home);
 
 //app setting
